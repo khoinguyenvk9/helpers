64b17f385cfefa6f7811d2260249eea6594f2b85
khoi.nguyen <khoi.nguyen@nexpando.com>
2023-08-18 11:32:01 +0700

    add remove duplicate object in array

--- fns/array.ts
+++ fns/array.ts
@@ -1,3 +1,5 @@
+import { TObject } from "./type";
+
 export function unionBy<T, K>(...arrays: (T[] | ((item: T) => K))[]): T[] {
   const iteratee = arrays.pop();
 
@@ -17,3 +19,26 @@ export function unionBy<T, K>(...arrays: (T[] | ((item: T) => K))[]): T[] {
     )(new Set<K>())
   );
 }
+
+export function removeDuplicatesByProperty<T extends TObject>(
+  array: T[],
+  property: keyof T
+): T[] {
+  if (!Array.isArray(array) || typeof property !== 'string') {
+    throw new Error('Invalid input');
+  }
+
+  const uniqueIds = new Set();
+  const unique: T[] = [];
+
+  for (const element of array) {
+    const propertyValue = element[property];
+    if (!uniqueIds.has(propertyValue)) {
+      uniqueIds.add(propertyValue);
+      unique.push(element);
+    }
+  }
+
+  return unique;
+}
+
